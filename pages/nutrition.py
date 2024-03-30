5d55d7d704a3283bcd1bfbb043f729ffda160e6d
Kulsoom Ahmad <kulsoom.ahmad@sjsu.edu>
2024-03-30 16:04:46 -0700

    nutrition v 3

--- pages/nutrition.py
+++ pages/nutrition.py
@@ -3,4 +3,36 @@ import openai
 import streamlit as st
 from openai import OpenAI
 
-st.image("food.jpeg")
+st.image("/Users/kulsoom/Milestone-2-v1/Milestone-2-v1/static/download.jpeg")
+
+openai.api_key = os.environ["APIKEY"]
+
+client = OpenAI()
+
+def get_completion(prompt, model="gpt-3.5-turbo"):
+   completion = client.chat.completions.create(
+        model=model,
+        messages=[
+        {"role":"system",
+         "content": "Your job is to information about nutrition health and dietary choices."},
+        {"role": "user",
+         "content": prompt},
+        ]
+    )
+   return completion.choices[0].message.content
+
+
+option = st.selectbox(
+   "What would you like to learn about?",
+   ("Importance of Balance", "Why are protiens important?", "Which Veggies?"),
+   index=None,
+   placeholder="Select a tab and start exploring",
+)
+
+if (option == "Importance of Balance"):
+    prompt = "Please explain the importance of eating balanced and healthy meals"
+
+    response = get_completion(prompt)
+
+
+    st.write(response)
